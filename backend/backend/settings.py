bd74330fc6943b4dc462feead87cfcb3d151def1
noSugarK <nosugark@qq.com>
2025-10-12 19:11:26 +0800

    docker: 修改docker配置文件

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,9 +29,7 @@ ALLOWED_HOSTS = ['8.148.253.79','localhost','127.0.0.1','172.27.105.3']
 
 # CORS设置
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-    "http://8.148.253.79:8080"
+    "http://8.148.253.79"
 ]
 
 # Application definition
